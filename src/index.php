2b72adf265ab96736f3594579c4ef134747a0d9b
Jeremy Seago <seagoj@gmail.com>
2012-12-07 15:45:13 -0600

    Commit dotfiles

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <link href="secure.php?file=bounce.css" type="text/css" rel="stylesheet" />
+        <link href="secure.php?file=color.css" type="text/css" rel="stylesheet" />
+        <link href="secure.php?file=font.css" type="text/css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <div>Test01234567890</div>
+        <div class="container">
+            <a href="mailto:seagoj@gmail.com" target="_blank">
+                <img src="secure.php?file=5_content_email16x16.png" class="bounce"/>
+            </a>
+        </div>
+    </body>
+</html>
