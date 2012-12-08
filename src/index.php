965171937ac28046ece2a779839db74ae43b8632
Jeremy Seago <seagoj@mail.seagoj.com>
2012-12-07 22:53:44 -0600

    Added multiple fonts.

--- src/index.php
+++ src/index.php
@@ -2,14 +2,14 @@
     <head>
         <link href="secure.php?file=bounce.css" type="text/css" rel="stylesheet" />
         <link href="secure.php?file=color.css" type="text/css" rel="stylesheet" />
-        <link href="secure.php?file=font.css" type="text/css" rel="stylesheet" />
+        <link href="secure.php?file=font-anonymous-pro.css" type="text/css" rel="stylesheet" />
     </head>
 
     <body>
         <div>Test01234567890</div>
         <div class="container">
             <a href="mailto:seagoj@gmail.com" target="_blank">
-                <img src="secure.php?file=5_content_email16x16.png" class="bounce"/>
+                <img src="secure.php?file=7_location_place16x16.png" class="bounce"/>
             </a>
         </div>
     </body>
